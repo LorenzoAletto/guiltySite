eda2d32df41681f95962ce89e614111035053d2b
tisanti147 <santid147@gmail.com>
2025-08-26 17:33:03 -0300

    footer fix

--- guilty-site/src/App.jsx
+++ guilty-site/src/App.jsx
@@ -1,12 +1,16 @@
 import './index.css'
 import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div>
-      <Header/>     
+      <Header/>
+      {/* <div>
+        <h1>Cuerpito</h1>
+      </div> */}
+      <Footer/> 
     </div>
-
   );
 }
 
--- guilty-site/src/components/footer.jsx
+++ guilty-site/src/components/footer.jsx
@@ -3,8 +3,8 @@ import './footer.css';
 
 const Footer = () => {
   return (
-    <footer class="footer">
-      <p>© 2025 My Website. All rights reserved.</p>
+    <footer className="p-5 bg-gray-800/40 text-center text-sm text-white absolute bottom-0 w-full">
+      <p>© 2025 Sega. All rights reserved.</p>
     </footer>
   );
 };
